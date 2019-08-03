634173a8430f29edbd337f5347f7a0b8b2b76544
Fadeev Ivan <githubfadeev@gmail.com>
2019-08-03 15:22:06 +0300

    creating header.js and Tehnologies.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import Tehnologies from'./Tehnologies.js';
 
-function App() {
-  return ( 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Make your dream! <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+   return (
+      <div className="App">
+        <Header/>
+        <Tehnologies/>
+      </div>
+    );
 }
+
+
 
 export default App;
