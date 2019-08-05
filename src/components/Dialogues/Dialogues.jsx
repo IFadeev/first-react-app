45ff8cb61cb0df1e34c4f0f2276e4b86800d9fff
Fadeev Ivan <githubfadeev@gmail.com>
2019-08-05 22:30:16 +0300

    Created Dialogues-base

--- src/components/Dialogues/Dialogues.jsx
+++ src/components/Dialogues/Dialogues.jsx
@@ -3,9 +3,84 @@ import dialog from './Dialogues.module.css'
 
 const Dialogues = (props) => {
   return (
-    <div className = {dialog.wrapper}>
-      <div className = {dialog.diaolgs__items}></div>
-      <div className = {dialog.messages}></div>
+    <div className = {dialog.wrapper}> 
+        <div className = {dialog.diaolgs__content}>
+
+
+          <div className = {dialog.diaolgs__item}>
+            <div className = {dialog.imager}>
+              <img className = {dialog.img} src={require("../../post-item-img.png")} ></img> 
+            </div>
+            <div className = {dialog.messages__content}>
+              <div className = {dialog.header}>Alex Bot</div>
+              <div className = {dialog.text}>Hello, world! You are amazing!</div>
+            </div>
+        </div>
+
+
+        <div className = {dialog.diaolgs__item}>
+            <div className = {dialog.imager}>
+              <img className = {dialog.img} src={require("../../post-item-img.png")} ></img> 
+            </div>
+            <div className = {dialog.messages__content}>
+              <div className = {dialog.header}>Alex Bot</div>
+              <div className = {dialog.text}>Hello, world! You are amazing!</div>
+            </div>
+        </div>
+
+
+      </div>
+      <div className = {dialog.messages}>
+        <div className = {dialog.messages__item}>
+
+          <div className = {dialog.imager}>
+              <img className = {dialog.img} src={require("../../post-item-img.png")} ></img> 
+            </div>
+            <div className = {dialog.messages__content}>
+              <div className = {dialog.header}>Alex Bot</div>
+              <div className = {dialog.text}>Hello, world! You are amazing!</div>
+            </div>
+          
+        </div>
+
+        <div className = {dialog.messages__item}>
+
+          <div className = {dialog.imager}>
+              <img className = {dialog.img} src={require("../../post-item-img.png")} ></img> 
+            </div>
+            <div className = {dialog.messages__content}>
+              <div className = {dialog.header}>Alex Bot</div>
+              <div className = {dialog.text}>Hello, world! You are amazing!</div>
+            </div>
+          
+        </div>
+
+        <div className = {dialog.messages__item}>
+
+          <div className = {dialog.imager}>
+              <img className = {dialog.img} src={require("../../post-item-img.png")} ></img> 
+            </div>
+            <div className = {dialog.messages__content}>
+              <div className = {dialog.header}>Alex Bot</div>
+              <div className = {dialog.text}>Hello, world! You are amazing!</div>
+            </div>
+          
+        </div>
+
+        <div className = {dialog.messages__item}>
+
+          <div className = {dialog.imager}>
+              <img className = {dialog.img} src={require("../../post-item-img.png")} ></img> 
+            </div>
+            <div className = {dialog.messages__content}>
+              <div className = {dialog.header}>Alex Bot</div>
+              <div className = {dialog.text}>Hello, world! You are amazing!</div>
+            </div>
+          
+        </div>
+
+
+      </div>
     </div>
   );
 }
