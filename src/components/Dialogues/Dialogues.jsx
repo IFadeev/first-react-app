550f1facf604195dfc33c3ec0c14f9c96b68e98f
Fadeev Ivan <githubfadeev@gmail.com>
2019-08-11 12:36:59 +0300

    Added React.createRef fro Dialogues component

--- src/components/Dialogues/Dialogues.jsx
+++ src/components/Dialogues/Dialogues.jsx
@@ -8,7 +8,15 @@ const Dialogues = (props) => {
   let mssageElement = props.dialogPage.messagesData.map( dialog => <Message fullName ={dialog.fullName} text={dialog.message}/>);
 
   let chatsDataElements = props.dialogPage.chatsData.map( dialog => <DialogueItem fullName={dialog.fullName} text={dialog.message} id={dialog.id}/>); 
-  
+
+  let newMessage = React.createRef();
+
+  let sendMessage = () => {
+    let text = newMessage.current.value;
+    text ? alert('Вы отпраили сообщение!: ' + text) :
+    alert('Сообщение пустое!')
+  }
+
   return (
     <div className={dialog.wrapper}>
       <div className={dialog.diaolgs__content}>
@@ -17,11 +25,20 @@ const Dialogues = (props) => {
 
       </div>
 
-      <div className={dialog.messages}>
+      <div className={dialog.messagesBlock}>
+        <div className={dialog.messages}>
 
-        {mssageElement}
+          {mssageElement}
+
+        </div>
+
+        <div className={dialog.textareaFild}>
+            <textarea ref={newMessage}  placeholder="Write here a message.."></textarea>
+            <button onClick ={ sendMessage} >Send</button>
+        </div>
 
       </div>
+
     </div>
   );
 }
