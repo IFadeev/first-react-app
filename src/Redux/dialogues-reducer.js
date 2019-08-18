2417a96d49e818cbba9e0665cb1e12f82a081c21
Fadeev Ivan <githubfadeev@gmail.com>
2019-08-18 11:29:49 +0300

    Added 'switch' in reducers

--- src/Redux/dialogues-reducer.js
+++ src/Redux/dialogues-reducer.js
@@ -2,25 +2,26 @@ const SEND_MESSAGE = 'SEND-MESSAGE';
 const UPDATE_NEW_MESSAGE = 'UPDATE-NEW-MESSAGE';
 
 const dialoguesReducer = (state, action) => {
-  if (action.type === SEND_MESSAGE) {
-    let newMessage = {
-      id: 5, 
-      name: 'Ivan',
-      surname: 'Fadeev',
-      message: state.currentMessage.message,
-      get fullName(){
-        return `${this.name} ${this.surname}`
-        }
-      };
-
-      state.messagesData.push(newMessage);
-      state.currentMessage.message = '';
-  }
-  else if (action.type === UPDATE_NEW_MESSAGE) {
-    state.currentMessage.message = action.message;
-  }
-
-  return state
+  switch (action.type) {
+    case SEND_MESSAGE:     
+      let newMessage = {
+        id: 5, 
+        name: 'Ivan',
+        surname: 'Fadeev',
+        message: state.currentMessage.message,
+        get fullName(){
+          return `${this.name} ${this.surname}`
+          }
+        };
+        state.messagesData.push(newMessage);
+        state.currentMessage.message = '';
+        return state
+    case UPDATE_NEW_MESSAGE:
+      state.currentMessage.message = action.message;
+      return state
+    default: 
+      return state
+  }  
 }
 
 export default dialoguesReducer;
--- src/Redux/profile-reducer.js
+++ src/Redux/profile-reducer.js
@@ -2,28 +2,27 @@ const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST = 'UPDATE-NEW-POST';
 
 const profileReducer = (state, action) => {
-
-  if (action.type === ADD_POST) {
-    //let nowDate = new Date();   
-    let newPost = { 
-      id: 5,
-      name: 'Ivan',
-      surname: 'Fadeev',
-      get fullName(){
-        return `${this.name} ${this.surname}`
-    },
-      message: state.currentPostData.message,
-      date: '08.11.19'
-  };
-    state.postData.unshift(newPost);
-    state.currentPostData.message = '';
-    
-  }
-
-  else if (action.type === UPDATE_NEW_POST) {
+  switch (action.type) {
+    case ADD_POST: 
+      let newPost = { 
+        id: 5,
+        name: 'Ivan',
+        surname: 'Fadeev',
+        get fullName(){
+          return `${this.name} ${this.surname}`
+      },
+        message: state.currentPostData.message,
+        date: '08.11.19'
+    };
+      state.postData.unshift(newPost);
+      state.currentPostData.message = '';
+      return state
+    case UPDATE_NEW_POST: 
       state.currentPostData.message = action.text;
+      return state
+    default: 
+      return state
   }
-  return state
 }
 
 export default profileReducer;
